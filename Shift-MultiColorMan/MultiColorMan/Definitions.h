51ad4515acf75a6d700ecda152d6fbefbb9e949c
dkrsorenson <dkrsorenson@gmail.com>
2019-11-04 18:03:39 -0500

    Initial project setup

---
+++ Shift-MultiColorMan/MultiColorMan/Definitions.h
@@ -0,0 +1,23 @@
+/*----------------------------------------------
+Team: Name Pending - Dakota, Sonya, David, Matt
+Date: 11/2019
+----------------------------------------------*/
+#ifndef __DEFINITIONS_H_
+#define __DEFINITIONS_H_
+
+//#include <vld.h>
+
+#include "Simplex\Simplex.h"
+
+#include "SFML\Window.hpp"
+#include "SFML\Graphics.hpp"
+#include "SFML\OpenGL.hpp"
+#include "SFML\Audio.hpp"
+
+#endif //__DEFINITIONS_H_
+
+/*
+USAGE:
+ARGUMENTS: ---
+OUTPUT: ---
+*/
---
+++ Shift-MultiColorMan/MultiColorMan/Main.h
@@ -0,0 +1,16 @@
+/*----------------------------------------------
+Team: Name Pending - Dakota, Sonya, David, Matt
+Date: 11/2019
+----------------------------------------------*/
+#ifndef __MAIN_H_
+#define __MAIN_H_
+
+#include "AppClass.h"
+
+#endif //__MAIN_H_
+
+/*
+USAGE:
+ARGUMENTS: ---
+OUTPUT: ---
+*/
